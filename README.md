cce6a0bac82f63975cb1532b7dc9e4587329cad9
LstChanceMadrid <michael.madrid@live.com>
2018-11-28 16:37:17 -0600

    added pg-promise

---
+++ README.md
@@ -0,0 +1,1 @@
+adding a server and database to react
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const pgp = require('pg-promise')();
+const connectionString = ""
+const db= pgp(connectionString);
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
